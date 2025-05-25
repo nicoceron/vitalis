741cc3509ee7a0b7b61766e687814191c044bbc4
Juan Torres <torrescjuan@javeriana.edu.co>
2025-05-25 11:40:41 -0500

    Update grid layout for payment section to ensure proper responsiveness

--- app/account/subscriptions/new/page.tsx
+++ app/account/subscriptions/new/page.tsx
@@ -251,7 +251,7 @@ export default function NewSubscriptionPage() {
 
           {/* 2) Payment & Shipping */}
           {step === "payment" && (
-            <div className="grid lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
               {/* Form */}
               <div className="lg:col-span-2">
                 <Card>
